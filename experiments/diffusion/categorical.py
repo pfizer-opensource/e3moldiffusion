283a2aae78830a527061a031ddf149607b1251cd
jule-c <jn.cremer@icloud.com>
2023-07-20 13:34:19 +0200

    add loss weighting, adaptive noise

--- experiments/diffusion/categorical.py
+++ experiments/diffusion/categorical.py
@@ -8,185 +8,216 @@ DEFAULT_ALPHAS = 1.0 - DEFAULT_BETAS
 ALPHAS_BAR = torch.cumprod(DEFAULT_ALPHAS, dim=0)
 
 
-def get_one_step_transition(alpha_t: float,
-                            terminal_distribution: torch.Tensor):
-  stay_prob = torch.eye(len(terminal_distribution)) * alpha_t
-  diffuse_prob = (1.0 - alpha_t) * (torch.ones(1, len(terminal_distribution)) * (terminal_distribution.unsqueeze(0)))
-  Q_t = stay_prob + diffuse_prob
-  return Q_t
+def get_one_step_transition(alpha_t: float, terminal_distribution: torch.Tensor):
+    stay_prob = torch.eye(len(terminal_distribution)) * alpha_t
+    diffuse_prob = (1.0 - alpha_t) * (
+        torch.ones(1, len(terminal_distribution)) * (terminal_distribution.unsqueeze(0))
+    )
+    Q_t = stay_prob + diffuse_prob
+    return Q_t
 
 
 class CategoricalDiffusionKernel(torch.nn.Module):
-  def __init__(self,
-               terminal_distribution: torch.Tensor,
-               alphas: torch.Tensor = DEFAULT_ALPHAS, 
-               ):
-    super().__init__()
-    
-    self.num_classes = len(terminal_distribution)
-    assert (terminal_distribution.sum() - 1.0).abs() < 1e-4
-    
-    self.register_buffer('eye', torch.eye(self.num_classes))
-    self.register_buffer('terminal_distribution', terminal_distribution)
-    self.register_buffer('alphas', alphas)
-    self.register_buffer('alphas_bar', torch.cumprod(alphas, dim=0))
-    self.register_buffer('one_minus_alphas_bar', 1.0 - self.alphas_bar)
-    Qt = [get_one_step_transition(alpha_t=a.item(), terminal_distribution=terminal_distribution) for a in alphas]
-    self.register_buffer('Qt', torch.stack(Qt, dim=0))
-    Qt_prev = torch.eye(self.num_classes)
-    Qt_bar = []
-    for i in range(len(alphas)):
-      Qtb = Qt_prev @ Qt[i]
-      Qt_bar.append(Qtb)
-      Qt_prev = Qtb
-    
-    Qt_bar = torch.stack(Qt_bar)
-    Qt_bar_prev = Qt_bar[:-1]
-    Qt_prev_pad = torch.eye(self.num_classes)
-    Qt_bar_prev = torch.concat([Qt_prev_pad.unsqueeze(0), Qt_bar_prev], dim=0)
-    self.register_buffer('Qt_bar', Qt_bar)
-    self.register_buffer('Qt_bar_prev', Qt_bar_prev)
-    
-    
-  def marginal_prob(self, x0: torch.Tensor, t: torch.Tensor):
-    """_summary_
-    Computes the forward categorical posterior q(xt | x0) ~ Cat(xt, p = x0_j . Qt_bar_ji)
-    Args:
-        x0 (torch.Tensor): _description_ one-hot vectors of shape (n, k)
-        t (torch.Tensor): _description_ time variable of shape (n,)
+    def __init__(
+        self,
+        terminal_distribution: torch.Tensor,
+        alphas: torch.Tensor = DEFAULT_ALPHAS,
+    ):
+        super().__init__()
 
-    Returns:
-        _type_: _description_
-    """  
-    
-    # Qt_bar (k0, k_t)
-    probs = torch.einsum('nj, nji -> ni', [x0, self.Qt_bar[t]])
-    check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
-    assert check
-    
-    return probs
-  
-  def reverse_posterior_for_every_x0(self, xt: torch.Tensor, t: torch.Tensor):
-    """_summary_
-    Computes the reverse posterior q(x_{t-1} | xt, x0) as described in Austin et al. (2021) https://arxiv.org/abs/2107.03006 in Eq.3
-    but for every possible value of x0
-    Args:
-        xt (torch.Tensor): _description_ a perturbed (noisy) one-hot vector of shape (n, k)
-        t (torch.Tensor): _description_ time variable of shape (n,)
-    Returns:
-        _type_: _description_
-    """
-      
-    # xt: (n, k_t)
-    
-    #x0 = torch.eye(self.num_classes, device=xt.device, dtype=xt.dtype).unsqueeze(0)
-    #x0 = x0.repeat((xt.size(0), 1, 1))
-    # (n, k, k)
-    
-    Qt_T = self.Qt[t]  # (n, k_t-1, k_t)
-    assert Qt_T.ndim == 3
-    Qt_T = Qt_T.permute(0, 2, 1)
-    # (n, k_t, k_t-1)
-    
-    a = torch.einsum('nj, nji -> ni', [xt, Qt_T])
-    # (n, k_t-1)
-    
-    a = a.unsqueeze(1)
-    # (n, 1, k_t-1)
-    
-    #b = torch.einsum('naj, nji -> nai', [x0, self.Qt_bar_prev[t]])
-    b = self.Qt_bar_prev[t]
-    # (n, k_0, k_t-1)
-    
-    p0 = a * b
-    # (n, k_0, k_t-1)
-    
-    # p1 = torch.einsum('naj, nji -> nai', [x0, self.Qt_bar[t]])
-    p1 = self.Qt_bar[t]
-    # (n, k_0, k_t)
-    
-    ## xt_ = xt.unsqueeze(1)
-    # (n, 1, k_t)
-  
-    ## p1 = (p1 * xt_).sum(-1, keepdims=True)
-    
-    p1 = torch.einsum('nij, nj -> ni', [p1, xt])
-    # (n, k_0)
-    
-    p1 = p1.unsqueeze(-1)
-    # (n, k_0, 1)
-    
-    probs = p0 / (p1.clamp(min=1e-5))
-    # (n, k_0, k_t-1)
-        
-    # check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
-    # assert check
-    
-    return probs
-  
-  def reverse_posterior(self, x0: torch.Tensor, xt: torch.Tensor, t: torch.Tensor):
-    """_summary_
-    Computes the reverse posterior q(x_{t-1} | xt, x0) as described in Austin et al. (2021) https://arxiv.org/abs/2107.03006 in Eq.3
-    Args:
-        x0 (torch.Tensor): _description_ one specific one-hot vector of shape (n, k)
-        xt (torch.Tensor): _description_ a perturbed (noisy) one-hot vector of shape (n, k)
-        t (torch.Tensor): _description_ time variable of shape (n,)
-    Returns:
-        _type_: _description_
-    """
-    a = torch.einsum('nj, nji -> ni', [xt, self.Qt[t].transpose(-2, -1)])
-    b = torch.einsum('nj, nji -> ni', [x0, self.Qt_bar_prev[t]])
-    p0 = a * b
-    # (n, k)
-    p1 = torch.einsum('nj, nji -> ni', [x0, self.Qt_bar[t]])
-    p1 = (p1 * xt).sum(-1, keepdims=True)
-    # (n, 1)
-    
-    probs = p0 / p1 
-    check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
-    assert check
-    
-    return probs
+        self.num_classes = len(terminal_distribution)
+        assert (terminal_distribution.sum() - 1.0).abs() < 1e-4
+
+        self.register_buffer("eye", torch.eye(self.num_classes))
+        self.register_buffer("terminal_distribution", terminal_distribution)
+        self.register_buffer("alphas", alphas)
+        self.register_buffer("alphas_bar", torch.cumprod(alphas, dim=0))
+        self.register_buffer("one_minus_alphas_bar", 1.0 - self.alphas_bar)
+        Qt = [
+            get_one_step_transition(
+                alpha_t=a.item(), terminal_distribution=terminal_distribution
+            )
+            for a in alphas
+        ]
+        self.register_buffer("Qt", torch.stack(Qt, dim=0))
+        Qt_prev = torch.eye(self.num_classes)
+        Qt_bar = []
+        for i in range(len(alphas)):
+            Qtb = Qt_prev @ Qt[i]
+            Qt_bar.append(Qtb)
+            Qt_prev = Qtb
+
+        Qt_bar = torch.stack(Qt_bar)
+        Qt_bar_prev = Qt_bar[:-1]
+        Qt_prev_pad = torch.eye(self.num_classes)
+        Qt_bar_prev = torch.concat([Qt_prev_pad.unsqueeze(0), Qt_bar_prev], dim=0)
+        self.register_buffer("Qt_bar", Qt_bar)
+        self.register_buffer("Qt_bar_prev", Qt_bar_prev)
+
+    def marginal_prob(self, x0: torch.Tensor, t: torch.Tensor):
+        """_summary_
+        Computes the forward categorical posterior q(xt | x0) ~ Cat(xt, p = x0_j . Qt_bar_ji)
+        Args:
+            x0 (torch.Tensor): _description_ one-hot vectors of shape (n, k)
+            t (torch.Tensor): _description_ time variable of shape (n,)
+
+        Returns:
+            _type_: _description_
+        """
+
+        # Qt_bar (k0, k_t)
+        probs = torch.einsum("nj, nji -> ni", [x0, self.Qt_bar[t]])
+        check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
+        assert check
+
+        return probs
+
+    def reverse_posterior_for_every_x0(self, xt: torch.Tensor, t: torch.Tensor):
+        """_summary_
+        Computes the reverse posterior q(x_{t-1} | xt, x0) as described in Austin et al. (2021) https://arxiv.org/abs/2107.03006 in Eq.3
+        but for every possible value of x0
+        Args:
+            xt (torch.Tensor): _description_ a perturbed (noisy) one-hot vector of shape (n, k)
+            t (torch.Tensor): _description_ time variable of shape (n,)
+        Returns:
+            _type_: _description_
+        """
+
+        # xt: (n, k_t)
+
+        # x0 = torch.eye(self.num_classes, device=xt.device, dtype=xt.dtype).unsqueeze(0)
+        # x0 = x0.repeat((xt.size(0), 1, 1))
+        # (n, k, k)
+
+        Qt_T = self.Qt[t]  # (n, k_t-1, k_t)
+        assert Qt_T.ndim == 3
+        Qt_T = Qt_T.permute(0, 2, 1)
+        # (n, k_t, k_t-1)
+
+        a = torch.einsum("nj, nji -> ni", [xt, Qt_T])
+        # (n, k_t-1)
+
+        a = a.unsqueeze(1)
+        # (n, 1, k_t-1)
+
+        # b = torch.einsum('naj, nji -> nai', [x0, self.Qt_bar_prev[t]])
+        b = self.Qt_bar_prev[t]
+        # (n, k_0, k_t-1)
+
+        p0 = a * b
+        # (n, k_0, k_t-1)
+
+        # p1 = torch.einsum('naj, nji -> nai', [x0, self.Qt_bar[t]])
+        p1 = self.Qt_bar[t]
+        # (n, k_0, k_t)
+
+        ## xt_ = xt.unsqueeze(1)
+        # (n, 1, k_t)
+
+        ## p1 = (p1 * xt_).sum(-1, keepdims=True)
+
+        p1 = torch.einsum("nij, nj -> ni", [p1, xt])
+        # (n, k_0)
+
+        p1 = p1.unsqueeze(-1)
+        # (n, k_0, 1)
+
+        probs = p0 / (p1.clamp(min=1e-5))
+        # (n, k_0, k_t-1)
+
+        # check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
+        # assert check
+
+        return probs
+
+    def reverse_posterior(self, x0: torch.Tensor, xt: torch.Tensor, t: torch.Tensor):
+        """_summary_
+        Computes the reverse posterior q(x_{t-1} | xt, x0) as described in Austin et al. (2021) https://arxiv.org/abs/2107.03006 in Eq.3
+        Args:
+            x0 (torch.Tensor): _description_ one specific one-hot vector of shape (n, k)
+            xt (torch.Tensor): _description_ a perturbed (noisy) one-hot vector of shape (n, k)
+            t (torch.Tensor): _description_ time variable of shape (n,)
+        Returns:
+            _type_: _description_
+        """
+        a = torch.einsum("nj, nji -> ni", [xt, self.Qt[t].transpose(-2, -1)])
+        b = torch.einsum("nj, nji -> ni", [x0, self.Qt_bar_prev[t]])
+        p0 = a * b
+        # (n, k)
+        p1 = torch.einsum("nj, nji -> ni", [x0, self.Qt_bar[t]])
+        p1 = (p1 * xt).sum(-1, keepdims=True)
+        # (n, 1)
+
+        probs = p0 / p1
+        check = torch.all((probs.sum(-1) - 1.0).abs() < 1e-4)
+        assert check
+
+        return probs
 
 
 def _some_debugging():
     num_classes = 5
-    uniform_distribution = torch.ones(num_classes, ) / num_classes
-    absorbing_distribution = torch.zeros(num_classes, )
+    uniform_distribution = (
+        torch.ones(
+            num_classes,
+        )
+        / num_classes
+    )
+    absorbing_distribution = torch.zeros(
+        num_classes,
+    )
     absorbing_distribution[0] = 1.0
-    absorbing_distribution = torch.tensor([9.5523e-01, 3.0681e-02, 2.0021e-03, 4.4172e-05, 1.2045e-02])
+    absorbing_distribution = torch.tensor(
+        [9.5523e-01, 3.0681e-02, 2.0021e-03, 4.4172e-05, 1.2045e-02]
+    )
 
-
-    atoms_drugs = [4.4119e-01, 1.0254e-06, 4.0564e-01, 6.4677e-02, 6.6144e-02, 4.8741e-03,
-            0.0000e+00, 9.1150e-07, 1.0847e-04, 1.2260e-02, 4.0306e-03, 0.0000e+00,
-            1.0503e-03, 1.9806e-05, 0.0000e+00, 7.5958e-08]
+    atoms_drugs = [
+        4.4119e-01,
+        1.0254e-06,
+        4.0564e-01,
+        6.4677e-02,
+        6.6144e-02,
+        4.8741e-03,
+        0.0000e00,
+        9.1150e-07,
+        1.0847e-04,
+        1.2260e-02,
+        4.0306e-03,
+        0.0000e00,
+        1.0503e-03,
+        1.9806e-05,
+        0.0000e00,
+        7.5958e-08,
+    ]
 
     edges_drugs = [9.5523e-01, 3.0681e-02, 2.0021e-03, 4.4172e-05, 1.2045e-02]
 
     atoms_qm9 = [0.5122, 0.3526, 0.0562, 0.0777, 0.0013]
     edges_qm9 = [0.8818, 0.1104, 0.0060, 0.0018, 0.0000]
-    
-    C0 = CategoricalDiffusionKernel(terminal_distribution=uniform_distribution,
-                                    alphas=DEFAULT_ALPHAS)
 
-    C1 = CategoricalDiffusionKernel(terminal_distribution=torch.tensor(edges_drugs),
-                                    alphas=DEFAULT_ALPHAS)
-    
-    
-    
+    C0 = CategoricalDiffusionKernel(
+        terminal_distribution=uniform_distribution, alphas=DEFAULT_ALPHAS
+    )
+
+    C1 = CategoricalDiffusionKernel(
+        terminal_distribution=torch.tensor(edges_drugs), alphas=DEFAULT_ALPHAS
+    )
+
     t = 290
     a = C0.Qt_bar[t]
     alphas_bar_t = C0.alphas_bar[t].unsqueeze(-1)
-    b =  alphas_bar_t * C0.eye \
-      + ((1.0 - alphas_bar_t) * torch.ones_like( C0.terminal_distribution)).unsqueeze(-1) * C0.terminal_distribution.unsqueeze(0)
-    
-    print(a-b)
-    
+    b = alphas_bar_t * C0.eye + (
+        (1.0 - alphas_bar_t) * torch.ones_like(C0.terminal_distribution)
+    ).unsqueeze(-1) * C0.terminal_distribution.unsqueeze(0)
+
+    print(a - b)
+
     alphas_t = C0.alphas[t].unsqueeze(-1)
     a = C0.Qt[t]
-    b = alphas_t * C0.eye \
-      + ((1.0 - alphas_t) * torch.ones_like( C0.terminal_distribution)).unsqueeze(-1) * C0.terminal_distribution.unsqueeze(0)
-    
-    print(a-b)
+    b = alphas_t * C0.eye + (
+        (1.0 - alphas_t) * torch.ones_like(C0.terminal_distribution)
+    ).unsqueeze(-1) * C0.terminal_distribution.unsqueeze(0)
+
+    print(a - b)
     Qt = C1.Qt[t]
     return None
--- experiments/diffusion/continuous.py
+++ experiments/diffusion/continuous.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
 import torch
 from torch import Tensor, nn
+
+
+def sigmoid(x):
+    return 1 / (np.exp(-x) + 1)
+
 
 def clip_noise_schedule(alphas2, clip_value=0.001):
     """
@@ -11,35 +15,37 @@ def clip_noise_schedule(alphas2, clip_value=0.001):
     """
     alphas2 = np.concatenate([np.ones(1), alphas2], axis=0)
 
-    alphas_step = (alphas2[1:] / alphas2[:-1])
+    alphas_step = alphas2[1:] / alphas2[:-1]
 
-    alphas_step = np.clip(alphas_step, a_min=clip_value, a_max=1.)
+    alphas_step = np.clip(alphas_step, a_min=clip_value, a_max=1.0)
     alphas2 = np.cumprod(alphas_step, axis=0)
 
     return alphas2
 
 
-def get_beta_schedule(beta_start: float = 1e-4,
-                      beta_end: float = 2e-2,
-                      num_diffusion_timesteps: int = 1000,
-                      kind: str = 'cosine',
-                      plot: bool = False, 
-                      **kwargs):
-
-    if kind == 'quad':
+def get_beta_schedule(
+    beta_start: float = 1e-4,
+    beta_end: float = 2e-2,
+    num_diffusion_timesteps: int = 1000,
+    kind: str = "cosine",
+    nu: float = 1.0,
+    plot: bool = False,
+    **kwargs
+):
+    if kind == "quad":
         betas = (
             torch.linspace(
-                beta_start ** 0.5,
-                beta_end ** 0.5,
+                beta_start**0.5,
+                beta_end**0.5,
                 num_diffusion_timesteps,
                 dtype=torch.get_default_dtype(),
             )
             ** 2
         )
-    elif kind == 'sigmoid':
+    elif kind == "sigmoid":
         betas = torch.linspace(-6, 6, num_diffusion_timesteps)
         betas = torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
-    elif kind == 'linear':
+    elif kind == "linear":
         betas = torch.linspace(beta_start, beta_end, num_diffusion_timesteps)
     elif kind == "cosine":
         s = kwargs.get("s")
@@ -47,7 +53,10 @@ def get_beta_schedule(beta_start: float = 1e-4,
             s = 0.008
         steps = num_diffusion_timesteps + 2
         x = torch.linspace(0, num_diffusion_timesteps, steps)
-        alphas_cumprod = torch.cos(((x / num_diffusion_timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+        alphas_cumprod = (
+            torch.cos(((x / num_diffusion_timesteps) + s) / (1 + s) * torch.pi * 0.5)
+            ** 2
+        )
         alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
         betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
         betas = torch.clip(betas, 0.0, 0.999)
@@ -57,16 +66,35 @@ def get_beta_schedule(beta_start: float = 1e-4,
         if s is None:
             s = 1e-4
         if p is None:
-            p = 3.
+            p = 3.0
         steps = num_diffusion_timesteps + 1
         x = np.linspace(0, steps, steps)
-        alphas2 = (1 - np.power(x / steps, p))**2
+        alphas2 = (1 - np.power(x / steps, p)) ** 2
         alphas2 = clip_noise_schedule(alphas2, clip_value=0.001)
         precision = 1 - 2 * s
         alphas2 = precision * alphas2 + s
         alphas = np.sqrt(alphas2)
         betas = 1.0 - alphas
         betas = torch.from_numpy(betas).float()
+    elif kind == "adaptive":
+        s = kwargs.get("s")
+        steps = num_diffusion_timesteps + 2
+        x = np.linspace(0, steps, steps)
+        x = np.expand_dims(x, 0)  # ((1, steps))
+
+        nu_arr = np.array(nu)  # (components, )  # X, charges, E, y, pos
+
+        alphas_cumprod = (
+            np.cos(0.5 * np.pi * (((x / steps) ** nu_arr) + s) / (1 + s)) ** 2
+        )  # ((components, steps))
+        # divide every element of alphas_cumprod by the first element of alphas_cumprod
+        alphas_cumprod_new = alphas_cumprod / alphas_cumprod[:, 0]
+        # remove the first element of alphas_cumprod and then multiply every element by the one before it
+        alphas = alphas_cumprod_new[:, 1:] / alphas_cumprod_new[:, :-1]
+
+        betas = 1 - alphas  # ((components, steps)) # X, charges, E, y, pos
+        betas = np.swapaxes(betas, 0, 1)
+        betas = torch.clip(torch.from_numpy(betas), 0.0, 0.999).squeeze()
 
     if plot:
         plt.plot(range(len(betas)), betas)
@@ -93,6 +121,7 @@ class DiscreteDDPM(nn.Module):
         N: int = 300,
         scaled_reverse_posterior_sigma: bool = True,
         schedule: str = "cosine",
+        nu: float = 1.0,
         enforce_zero_terminal_snr: bool = False,
         **kwargs
     ):
@@ -107,21 +136,33 @@ class DiscreteDDPM(nn.Module):
         self.beta_max = beta_max
         self.N = N
         self.scaled_reverse_posterior_sigma = scaled_reverse_posterior_sigma
-        
-        assert schedule in ["linear", "quad", "cosine", "sigmoid", "polynomial"]
-        
-        discrete_betas = get_beta_schedule(beta_start=beta_min, beta_end=beta_max,
-                                           num_diffusion_timesteps=N,
-                                           kind=schedule, plot=False)
-        
+
+        assert schedule in [
+            "linear",
+            "quad",
+            "cosine",
+            "sigmoid",
+            "polynomial",
+            "adaptive",
+        ]
+
+        discrete_betas = get_beta_schedule(
+            beta_start=beta_min,
+            beta_end=beta_max,
+            num_diffusion_timesteps=N,
+            kind=schedule,
+            nu=nu,
+            plot=False,
+        )
+
         if enforce_zero_terminal_snr:
             discrete_betas = self.enforce_zero_terminal_snr(betas=discrete_betas)
-        
+
         self.enforce_zero_terminal_snr = enforce_zero_terminal_snr
-        
+
         sqrt_betas = torch.sqrt(discrete_betas)
         alphas = 1.0 - discrete_betas
-            
+
         sqrt_alphas = torch.sqrt(alphas)
         alphas_cumprod = torch.cumprod(alphas, dim=0)
         alphas_cumprod_prev = torch.nn.functional.pad(
@@ -148,7 +189,7 @@ class DiscreteDDPM(nn.Module):
         self.register_buffer("sqrt_alphas_cumprod", sqrt_alphas_cumprod)
         self.register_buffer("sqrt_1m_alphas_cumprod", sqrt_1m_alphas_cumprod)
         self.register_buffer("reverse_posterior_sigma", reverse_posterior_sigma)
-        
+
     def enforce_zero_terminal_snr(self, betas):
         # Convert betas to alphas_bar_sqrt
         alphas = 1 - betas
@@ -166,7 +207,7 @@ class DiscreteDDPM(nn.Module):
         alphas_bar_sqrt *= alphas_bar_sqrt_0 / (alphas_bar_sqrt_0 - alphas_bar_sqrt_T)
 
         # Convert alphas_bar_sqrt to betas
-        alphas_bar = alphas_bar_sqrt ** 2
+        alphas_bar = alphas_bar_sqrt**2
         alphas = alphas_bar[1:] / alphas_bar[:-1]
         alphas = torch.cat([alphas_bar[0:1], alphas])
         betas = 1 - alphas
@@ -183,19 +224,19 @@ class DiscreteDDPM(nn.Module):
             _type_: _description_
         """
         expand_axis = len(x.size()) - 1
-        
+
         assert str(t.dtype) == "torch.int64"
         signal = self.sqrt_alphas_cumprod[t]
         std = self.sqrt_1m_alphas_cumprod[t]
-        
+
         for _ in range(expand_axis):
             signal = signal.unsqueeze(-1)
             std = std.unsqueeze(-1)
 
         mean = signal * x
-        
+
         return mean, std
-    
+
     def plot_signal_to_noise(self):
         t = torch.arange(0, self.N)
         signal = self.alphas_cumprod[t]
@@ -205,54 +246,56 @@ class DiscreteDDPM(nn.Module):
         plt.xlabel("timesteps")
         plt.legend()
         plt.show()
-  
-  
-  
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     T = 500
     schedule = "cosine"
-    
-    sde = DiscreteDDPM(beta_min=1e-4,
-                       beta_max=2e-2,
-                       N=T,
-                       scaled_reverse_posterior_sigma=True, 
-                       schedule=schedule,
-                       enforce_zero_terminal_snr=False)
-    
+
+    sde = DiscreteDDPM(
+        beta_min=1e-4,
+        beta_max=2e-2,
+        N=T,
+        scaled_reverse_posterior_sigma=True,
+        schedule=schedule,
+        enforce_zero_terminal_snr=False,
+    )
+
     sde.plot_signal_to_noise()
-    
+
     plt.plot(range(len(sde.discrete_betas)), sde.discrete_betas, label="betas")
     plt.plot(range(len(sde.alphas)), sde.alphas, label="alphas")
     plt.xlabel("t")
     plt.legend()
     plt.show()
-    
+
     #### SNR
     # equation 1 and 2 in https://arxiv.org/pdf/2107.00630.pdf
     signal = sde.alphas_cumprod  # \alpha_t
-    noise = 1.0 - signal  # \sigma_t^2 
+    noise = 1.0 - signal  # \sigma_t^2
     snr = signal / noise
     plt.plot(range(len(signal)), snr, label="SNR(t)")
     plt.legend()
     plt.show()
-    
+
     #### notation from kingma
     gamma = torch.log(snr)
     plt.plot(range(len(signal)), gamma, label="gamam(t) = log_e(SNR(t))")
     plt.legend()
     plt.show()
-    
+
     ### From https://arxiv.org/pdf/2303.00848.pdf
-    # sigmoidal    
+    # sigmoidal
     plt.plot(gamma, torch.sigmoid(-gamma + 2), label="sigmoidal-weights")
     plt.legend()
     plt.show()
-    
+
     plt.plot(gamma, torch.sigmoid(-gamma + 2), label="sigmoidal-weights")
     plt.legend()
     plt.show()
-    
-    plt.plot(torch.arange(len(gamma)), torch.sigmoid(-gamma + 2), label="sigmoidal-weights")
+
+    plt.plot(
+        torch.arange(len(gamma)), torch.sigmoid(-gamma + 2), label="sigmoidal-weights"
+    )
     plt.legend()
     plt.show()
