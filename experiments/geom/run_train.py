c767dd9fcac31663816dd762dd8aeb9d64336d1e
tuanle618 <tuanle@hotmail.de>
2023-08-08 15:33:37 +0200

    try vae

--- experiments/geom/run_train.py
+++ experiments/geom/run_train.py
@@ -69,8 +69,8 @@ if __name__ == "__main__":
         from experiments.bond_prediction_discrete import Trainer
     elif hparams.latent_dim:
         print("Using latent diffusion")
-        # from experiments.diffusion_latent_discrete_vae import Trainer
-        from experiments.diffusion_latent_discrete_diff import Trainer
+        from experiments.diffusion_latent_discrete_vae import Trainer
+        # from experiments.diffusion_latent_discrete_diff import Trainer
         # from experiments.diffusion_latent_discrete_flow import Trainer
     else:
         print("Using discrete diffusion")
